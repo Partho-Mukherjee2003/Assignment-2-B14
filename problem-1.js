5a5e97fc80d53c20fa8a24b91b8758396b85e30f
Partho-Mukherjee2003 <parthomukherjee582@gmail.com>
2026-07-30 00:02:04 +0600

    Problem 3 done

--- problem-1.js
+++ problem-1.js
@@ -1,6 +1,6 @@
 
 
-function matchWinner(teamAGoals, teamBGoals) {
+function matchWinner(teamAGoals=0, teamBGoals=0) {
   if (typeof teamAGoals !== 'number' || typeof teamBGoals !== 'number') {
     return 'Invalid';
 }
@@ -17,4 +17,13 @@ function matchWinner(teamAGoals, teamBGoals) {
   }
 
 }
-
+let result1 = matchWinner(3, 2);
+console.log(result1);
+let result2 = matchWinner(1, 4);
+console.log(result2);
+let result3 = matchWinner(2, 2);
+console.log(result3);
+let result4 = matchWinner("3", 2);
+console.log(result4);
+let result5 = matchWinner(3);
+console.log(result5);
---
+++ problem-3.js
@@ -0,0 +1,26 @@
+function calculateAiCost(tokensUsed) {
+  if (typeof tokensUsed !== 'number' || tokensUsed < 0) {
+    return'Invalid';
+  }
+  else{
+    if (tokensUsed <= 500 )
+      return 0
+    else {
+      let ChargerAvailableToken = tokensUsed - 500;
+      let tokenCost = Math.floor(ChargerAvailableToken/100)
+      return (tokenCost * 5)
+    }
+  }
+}
+let result1 = calculateAiCost(300);
+console.log(result1);
+let result2 = calculateAiCost(500);
+console.log(result2);
+let result3 = calculateAiCost(650);
+console.log(result3);
+let result4 = calculateAiCost(1000);
+console.log(result4);
+let result5 = calculateAiCost(-10);
+console.log(result5);
+let result6 = calculateAiCost("500");
+console.log(result6);
